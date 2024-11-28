10e9b703c5f39e8eeb47fc7645edf15ccd9a2c71
Raymond <raymondjohnson@pursuit.org>
2024-11-27 22:59:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from './components/home'
+import Nav from './components/nav.jsx'
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>my_legal_fe</h1>
+      <Nav/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './nav.css';
+
+const Nav = () => {
+  return (
+    <ul className='nav'>
+      <li className="nav-item">home</li>
+      <li className="nav-item">depos</li>
+      <li className="nav-item">my notes</li>
+      <li className="nav-item">links</li>
+    </ul>
+  )
+}
+
+export default Nav
